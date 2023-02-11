3d2c4f59d8ce330ae984758d44b28331ae963803
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2023-02-11 20:50:03 -0300

    model da rota produto adicionada

---
+++ src/models/Produto.js
@@ -0,0 +1,10 @@
+class Produto{
+    constructor(nome, descricao, id, preco){
+        this.nome = nome
+        this.descricao = descricao
+        this.id = id
+        this.preco = preco
+    }
+}
+
+module.exports = Produto
