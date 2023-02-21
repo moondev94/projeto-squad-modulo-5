9bac5e528920cd681ad288b2000fc9d3e41ef350
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2023-02-21 02:42:13 -0300

    Atualiza o arquivo app.js

--- src/app.js
+++ src/app.js
@@ -4,14 +4,19 @@ const express = require('express')
 // instanciando o servidor
 const app = express()
 
+// Importa o cors
+const cors = require('cors')
+
 // configurando o servidor para receber requisições com o corpo no formato JSON
 app.use(express.json())
 
-// importando os controllers
-const produtoController = require('./controllers/produtoController.js')
-const entregaController = require('./controllers/entregaController.js')
+app.use(cors())
 
+// importando os controllers
+const clienteController = require('./controllers/clienteController.js')
+const produtoController = require('./controllers/produtoController.js')
+
+clienteController.rotas(app)
 produtoController.rotas(app)
-entregaController.rotas(app)
 
 module.exports = app
