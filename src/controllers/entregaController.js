6e93193f8f44aeddfa96bdd682882869efee0c5c
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2023-02-22 23:14:20 -0300

    Atualiza o arquivo entregaController.js

--- src/controllers/entregaController.js
+++ src/controllers/entregaController.js
@@ -1,26 +1,78 @@
-// entregas não está completa. Falta implementar as funcionalidades
 
-// Importa o bd.js para poder usar o banco de dados simulado
-const { bdEntregas } = require("../infra/bd.js")
+const EntregaDAO = require('../DAO/EntregaDAO')
+const Entrega = require('../models/Entrega')
 
 class entregaController {
     static rotas(app){
-        // Rota para o recurso entrega
         app.get('/entrega', entregaController.listar)
+        app.get('/entrega/id/:id', entregaController.buscarPorID)
         app.post('/entrega', entregaController.inserir)
+        app.put('/entrega/id/:id', entregaController.atualizar)
+        app.delete('/entrega/id/:id', entregaController.deletar)
     }
 
-    static listar(req, res){
-        const entregas = bdEntregas
-        // Devolve a lista de entregas
-        res.send(entregas)
+
+    static async listar(req, res){
+        const entrega = await EntregaDAO.listar()
+
+        res.status(200).send(entrega)
     }
 
-    static inserir(req, res){
-        res.send('Rota ativada com POST e recurso entrega: entrega deve ser inserida')
-        // Console log do corpo da requisição
-        console.log(req.body)
+    static async buscarPorID(req, res){
+        const entrega = await EntregaDAO.buscarPorID(req.params.id)
+
+    
+        if(!entrega){
+            res.status(404).send('Informações de entrega não encontradas no banco.')
+        }
+    
+        res.status(200).send(entrega)
+
+    }
+
+    static async inserir(req, res){
+        const entrega = {
+            id: req.body.id,
+            endereco: req.body.endereco,
+            cliente: req.body.cliente,
+            id_produto: req.body.id_produto,
+            id_cliente: req.body.id_cliente
+        }
+
+        const result = await EntregaDAO.inserir(entrega)
+
+        res.status(201).send({"Mensagem": "Informações de entrega incluídas com sucesso!", "Nova entrega: ": result})    
+    }
+
+
+    static async atualizar(req, res) {
+        const entrega = new Entrega(req.body.id, req.body.endereco, req.body.cliente, req.body.id_produto, req.body.id_cliente)
+
+        if (!entrega) {
+            res.status(404).send('Informações de entrega não encontradas no banco')
+        }
+        const result = await EntregaDAO.atualizar(req.params.id, entrega)
+        if(result.erro){
+            res.status(500).send('Erro ao atualizar as informações de entrega no banco')
+          }
+
+
+        res.status(201).send({"Mensagem": "Informações de entrega atualizadas!", "Nova entrega: ": entrega})
+    }
+
+
+    static async deletar(req, res) {
+        const entrega = await EntregaDAO.deletar(req.params.id)
+
+        if(!entrega){
+            res.status(404).send('Informações de entrega não encontradas no banco')
+        }
+
+        res.status(204).send({"Mensagem": 'As informaçoes de entrega foram excluídas'})
     }
 }
 
+
 module.exports = entregaController
+
+
